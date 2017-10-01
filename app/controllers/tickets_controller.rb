5392a28c61d7a87fc0b8a6f894f492280f27cc1c
Yanko Ivanov <yanko.ivanov@onmoon.org>
2017-10-01 17:44:19 +0300

    Handle ticket-not-found, decouple JSON schema from model

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,16 +1,32 @@
 class TicketsController < ActionController::API
   include ActionView::Helpers::NumberHelper
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def create
     ticket = Ticket.new
     ticket.save
 
-    render json: ticket
+    render_ticket(ticket)
   end
 
   def show
-    ticket = Ticket.where(barcode: params[:id]).take
+    ticket = Ticket.where(barcode: params[:id]).take!
 
-    render json: number_to_currency(ticket.fee)
+    render_ticket(ticket)
+  end
+
+  private
+
+  def render_ticket(ticket)
+    render json: {
+      barcode: ticket.barcode,
+      issued_at: ticket.issued_at,
+      fee: number_to_currency(ticket.fee)
+    }
+  end
+
+  def record_not_found(e)
+    render json: { status: 404, message: "The requested resource was not found" }, status: 404
   end
 end

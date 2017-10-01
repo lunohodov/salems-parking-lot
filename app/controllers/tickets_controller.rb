85eaf5b91e3fcad1fde0e032700014ef67710571
Yanko Ivanov <yanko.ivanov@onmoon.org>
2017-10-01 14:57:17 +0300

    Implement endpoint for ticket's fee `GET /tickets/:barcode`

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,16 @@
 class TicketsController < ActionController::API
+  include ActionView::Helpers::NumberHelper
+
   def create
     ticket = Ticket.new
     ticket.save
 
     render json: ticket
+  end
+
+  def show
+    ticket = Ticket.where(barcode: params[:id]).take
+
+    render json: number_to_currency(ticket.fee)
   end
 end
--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -1,10 +1,23 @@
 require 'test_helper'
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
+  include ActionView::Helpers::NumberHelper
+
   test "should create a new ticket" do
     post tickets_path
 
     assert_response :success
     assert_equal "application/json", @response.content_type
   end
+
+  test "should provide the fee due for a specified ticket" do
+    t = Ticket.create
+
+    get ticket_path(t.barcode)
+
+    assert_response :success
+    assert_equal "application/json", @response.content_type
+    assert_equal number_to_currency(t.fee), @response.body
+  end
 end
+

9897d174bf53fd62413f25c8f3406f628317bb41
Yanko Ivanov <yanko.ivanov@onmoon.org>
2017-10-02 11:09:12 +0300

    Put common JSON asserts into test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,15 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def assert_json_response(spec = {}, &block)
+    assert_equal "application/json", @response.content_type
+
+    json = ActiveSupport::JSON.decode(@response.body).symbolize_keys
+
+    spec.each do |k, v|
+      assert_equal v, json[k.to_sym], "Field mismatch #{k}"
+    end
+
+    yield(json, spec) if block_given?
+  end
 end

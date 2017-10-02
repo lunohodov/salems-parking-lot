a548e75dfd88e4f9c3bd16cad4e620c4c4f31f88
Yanko Ivanov <yanko.ivanov@onmoon.org>
2017-10-02 09:38:37 +0300

    Refactor error handling in API controllers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,21 @@
 class ApiController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from StandardError, with: :handle_error
 
   private
 
-  def record_not_found(e)
-    render json: { status: 404, message: "The requested resource was not found" }, status: 404
+  def handle_error(e)
+    # FIXME
+    logger.error e
+
+    case e
+      when ActiveRecord::RecordNotFound
+        render_error 404, "The requested resource was not found"
+      else
+        render_error 500, "An unexpected error occurred"
+    end
+  end
+
+  def render_error(status, message)
+    render json: { status: status, message: message }, status: status
   end
 end
